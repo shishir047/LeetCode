97da0a45e2cac4f26cdf4cb71349153ce96ecf8e
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-04-04 03:43:00 -0700

    Add files via upload

---
+++ majority_number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int flag = 1;
+        int i;
+        for (i = 0; i < nums.length; i++) {
+
+            try {
+                if (nums[i] == nums[i + 1]) {
+                    flag++;
+                    continue;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+            }
+
+            if (flag <= nums.length / 2) {
+                flag = 1;
+            }else {
+                return nums[i];
+            }
+        }
+
+        return flag;
+    }
+}
