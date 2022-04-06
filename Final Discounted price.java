68770bd5eeb1e0049931813c462de670113ea763
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-04-06 05:25:33 -0700

    Add files via upload

---
+++ Final Discounted price.java
@@ -0,0 +1,42 @@
+package io.github.shishir;
+
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        int array[] = {10,1,1,6}; // Output: [9,1,1,6]
+        System.out.println(Arrays.toString(main.finalPrices(array)));
+    }
+
+    public int[] finalPrices(int[] prices) {
+
+        int[] fPrice = new int[prices.length];
+        for(int i=0; i< prices.length ; i++){
+            if(i==prices.length-1){
+                fPrice[i] = prices[i];
+            }
+            for(int j=i+1; j< prices.length ; j++){
+
+                if(prices[j]<=prices[i]){
+                    fPrice[i] = prices[i]-prices[j];
+                    break;
+                }else if(j == prices.length-1){
+                    fPrice[i] = prices[i];
+                    break;
+                }
+
+
+            }
+
+
+
+
+        }
+
+
+
+return fPrice;
+    }
+
+}
