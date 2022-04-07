924f024de865c3d19c6cf8cd79bf87dbf29f2c23
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-04-07 06:51:12 -0700

    Add files via upload

---
+++ Create Target Array in the Given Order.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            target[i] = Integer.MIN_VALUE;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(target[index[i]]==Integer.MIN_VALUE){
+                target[index[i]] = nums[i];
+            }else{
+                for(int j = target.length-1; j > index[i]; j--) {
+                    target[j] = target[j - 1];
+                }
+                target[index[i]] = nums[i];
+
+            }
+        }
+
+        return target;
+    }
+}
