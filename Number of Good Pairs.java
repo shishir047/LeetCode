7d3662cd79ac7ae9575b23c2cfa61cdaf4814c4e
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-04-08 08:31:12 -0700

    Add files via upload

---
+++ Number of Good Pairs.java
@@ -0,0 +1,11 @@
+public int numIdenticalPairs(int[] nums) {
+        int flag =0;
+        for(int i=0; i<nums.length ; i++ ){
+            for (int j=i+1; j<nums.length; j++){
+                if(i<j && nums[i]==nums[j]){
+                    flag++;
+                }
+            }
+        }
+    return flag;
+    }
