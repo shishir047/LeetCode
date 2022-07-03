b3094105ce7e1118ed56f8abbad599f898748ded
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-07-03 20:46:19 +0530

    Update Build Array from Permutation.java

--- Build Array from Permutation.java
+++ Build Array from Permutation.java
@@ -1,5 +1,5 @@
  class Solution {
-    public int[] buildArray(int[] nums) {
+    public int[] buildArray(int[] nums) { 
         int[] ans = new int[nums.length];
         for(int i=0; i<nums.length; i++){ 
             ans[i] = nums[nums[i]];   
