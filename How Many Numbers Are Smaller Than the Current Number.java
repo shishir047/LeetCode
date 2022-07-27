fd160cb5e1e1ef3c2b78f54e77e2ae666ae82391
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-07-28 02:31:03 +0530

    Update How Many Numbers Are Smaller Than the Current Number.java

--- How Many Numbers Are Smaller Than the Current Number.java
+++ How Many Numbers Are Smaller Than the Current Number.java
@@ -4,7 +4,7 @@ class Solution {
         int[] arr =new int[nums.length];
         for(int i=0; i<nums.length; i++){
             for(int j=0; j<nums.length; j++){
-                if(nums[i]> nums[j]){
+                if(nums[i]> nums[j]){ 
                     flag++;
                 }
 
