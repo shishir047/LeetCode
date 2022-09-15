f5e398e1047359bcfbadde0a746eea454b751876
Shishir Kumar Singh <shishir47singh@gmail.com>
2022-09-15 06:42:29 -0400

    Add files via upload

---
+++ 268. MissingNumber.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int flag=0;
+        int a=0;
+        Arrays.sort(nums);
+        for(int i=0; i<len; i++){
+            if(i==(len-1) && nums[i]==i){
+                a=len;
+                break;
+            }else{ 
+            
+                 if(nums[i]==i){
+                        continue;
+                    }else{
+                        a=i;
+                        break;
+                 }
+            }
+        }
+        return a;
+    }
+}
