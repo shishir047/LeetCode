95284afc1336262ec8f1da428345ea49ebaec11f
Shishir Kumar Singh <shishir47singh@gmail.com>
2023-01-16 23:03:36 -0500

    Update Determine Whether Matrix Can Be Obtained By Rotation.java

--- Determine Whether Matrix Can Be Obtained By Rotation.java
+++ Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -11,7 +11,7 @@ class Solution {
             
         
             for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix.length; j++) {
+                for (int j = 0; j < matrix.length; j++) { 
                     newMatrix[i][j] = matrix[j][i];
                 }
             }   
