a6620165f1b85c722a60e9ef6ef2836fcdb091aa
Shishir Kumar Singh <shishir47singh@gmail.com>
2023-02-06 13:21:56 -0500

    Update Build Array from Permutation.java

--- Build Array from Permutation.java
+++ Build Array from Permutation.java
@@ -3,7 +3,7 @@
         int[] ans = new int[nums.length];
         for(int i=0; i<nums.length; i++){  
             ans[i] = nums[nums[i]];      
-        } 
+        } v
         return ans; 
     } 
 }   
