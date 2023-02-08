95062ec1280373d07b92a3dc1dc0425b66b46258
Shishir Kumar Singh <shishir47singh@gmail.com>
2023-02-08 03:12:35 -0500

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -5,7 +5,7 @@ class Solution {
                 while(num>0){
                     rev = (rev*10)+(num%10);
                     num=num/10;
-                }
+                } 
 
                 if(rev == x){
                     return true;
